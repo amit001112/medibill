ebe913025311d04c92d5473d0deceeab718dea75
amit001112 <lifelineemergencycare@gmail.com>
2025-07-30 22:01:26 +0530

    Update db.ts

--- server/db.ts
+++ server/db.ts
@@ -2,8 +2,8 @@ import { drizzle } from "drizzle-orm/better-sqlite3";
 import Database from "better-sqlite3";
 import * as schema from "./shared/schema";
 
-// Point to your SQLite file (hospital.db)
-const sqlite = new Database("hospital.db");
+// Point to your SQLite file inside the server folder
+const sqlite = new Database("server/hospital.db");
 
 // Export drizzle instance
 export const db = drizzle(sqlite, { schema });
